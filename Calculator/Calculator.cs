9deb505dc8290838b36c9763cf85b61494d90b4a
maxfriman <maxappdevelopement@gmail.com>
2019-11-08 15:19:23 +0100

    First commit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+        public double NumberInMemory { get; set; }
+        
+        public Calculator()
+        {
+            FirstNumber = 0;
+            SecondNumber = 0;
+            NumberInMemory = 0;
+        }
+        
+        public double SumNumbers() => FirstNumber + SecondNumber;
+
+        public double MultiplyNumbers() => FirstNumber * SecondNumber;
+
+        public double SumInBetween()
+        {
+            double sum = 0;
+            double smallestNumber = FirstNumber >= SecondNumber ? SecondNumber : FirstNumber;
+            double largestNumber = FirstNumber <= SecondNumber ? SecondNumber : FirstNumber;
+            
+            for (double i = smallestNumber; i <= largestNumber; i++)
+            {
+                sum += smallestNumber;
+                smallestNumber++;
+            }
+            return sum;
+        }
+
+        public void AddNumberToMemory(double num) 
+        {
+            NumberInMemory = num;
+        }
+
+        public override string ToString()
+        {
+            return $"First number: {FirstNumber}\n" +
+                $"Second number: {SecondNumber}\n" +
+                $"Number in memory: {NumberInMemory}";
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Calculator
+{
+    class MainClass
+    {
+        public static Calculator calculator = new Calculator();
+
+        public static void Main(string[] args)
+        {
+            bool looping = true;
+            while (looping)
+            {
+                Console.WriteLine("1. Add two numbers\n" +
+                              "2. Multiply two numbers\n" +
+                              "3. Sum the numbers between two numbers\n" +
+                              "4. Add number to memory\n" +
+                              "5. Clear memory\n" +
+                              "6. Print calculator contents\n" +
+                              "7. Quit\n");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Add();
+                        break;
+                    case "2":
+                        Multiply();
+                        break;
+                    case "3":
+                        SumInBetween();
+                        break;
+                    case "4":
+                        NumberInMemory();
+                        break;
+                    case "5":
+                        ClearMemory();
+                        break;
+                    case "6":
+                        PrintCalculatorContents();
+                        break;
+                    case "7":
+                        Console.WriteLine("Goodbye");
+                        looping = false;
+                        break;
+                }
+            }
+        }
+
+        private static void Add()
+        {
+            SetNumbersToCalculator("Print two numbers to add");
+            Console.WriteLine($"Sum of numbers is {calculator.SumNumbers()}");
+        }  
+
+        private static void Multiply()
+        {
+            SetNumbersToCalculator("Print two numbers to multiply");
+            Console.WriteLine($"Product of numbers is {calculator.MultiplyNumbers()}\n");            
+        }
+
+        private static void SumInBetween()
+        {
+            SetNumbersToCalculator("Print two numbers to sum in between");
+            Console.WriteLine($"Sum of numbers in between is {calculator.SumInBetween()}\n");            
+        }
+
+        private static void NumberInMemory()
+        {
+            Console.WriteLine("Add a number to memory");
+            double numInMemory;
+            if (double.TryParse(Console.ReadLine(), out numInMemory))
+            {
+                calculator.AddNumberToMemory(numInMemory);
+                Console.WriteLine("Number successfully added to memory");
+            }
+            else
+            {
+                Console.WriteLine("You must only print a number");
+            }            
+        }
+
+        private static void ClearMemory()
+        {
+            calculator = new Calculator();
+            Console.WriteLine("Number in memory cleared");
+        }
+       
+        private static void PrintCalculatorContents() => Console.WriteLine(calculator);
+        
+        private static void SetNumbersToCalculator(string message)
+        {
+            Console.WriteLine(message);       
+            double firstNumber, secondNumber;
+            if (double.TryParse(Console.ReadLine(), out firstNumber) && double.TryParse(Console.ReadLine(), out secondNumber))
+            {
+                calculator.FirstNumber = firstNumber;
+                calculator.SecondNumber = secondNumber;
+            }
+            else
+            {
+                Console.WriteLine("You must only print numbers");
+            }
+        }
+    }  
+}
